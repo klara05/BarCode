2ce703d312c18d15364acc7b174816717560444d
Klara Minga <klara.minga@adplastik.hr>
2021-06-27 22:52:40 +0200

    Add project files.

---
+++ BarCode/BarCode/Views/ScanPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarCode.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarCode.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScanPage : ContentPage
+    {
+        ScanViewModel viewModel;
+        public ScanPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new ScanViewModel();
+        }
+        public void scanView_OnScanResult(Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned result", "The barcode's text is " + result.Text + ". The barcode's format is " + result.BarcodeFormat, "OK");
+            });
+        }
+
+        
+    }
+}
